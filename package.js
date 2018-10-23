690094dc155ffbb14df32c75c6ee8f41fdd55555
Jan Küster <jkuester@uni-bremen.de>
2018-10-23 17:19:15 +0200

    updated package info

--- package.js
+++ package.js
@@ -4,19 +4,19 @@ Package.describe({
   name: 'jkuester:autoform-hint',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Add hints to your quickForms using SimpleSchema.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jankapunkt/meteor-autoform-hint.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.8')
+  api.versionsFrom('1.6')
   api.use('ecmascript')
-  api.use('templating')
-  api.use('aldeed:autoform')
+  api.use('templating@1.3.2')
+  api.use('aldeed:autoform@6.3.0')
   api.addFiles([
     'autoform-hint.html',
     'autoform-hint.js'
